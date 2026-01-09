84103efcec202f22b9d931cf317238a3da66c1c1
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-09 10:01:09 +0200

    chore: export Navbar component from index
    - Added named export for Navbar from './Navbar'
    - Simplifies imports across the project by centralizing component exports

---
+++ src/components/layout/navbar/components/index.ts
@@ -0,0 +1,5 @@
+export { Logo } from './Logo';
+export { SearchBar } from './SearchBar';
+export { NavigationLinks } from './NavigationLinks';
+export { LanguageToggle } from './LanguageToggle';
+export { UserMenu } from './UserMenu';
