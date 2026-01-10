21eb7194bc7bd7b364f55ba5f10696e7c46467c7
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-10 06:18:42 +0200

    chore: create central export file for product listing components
    - Consolidates exports for PageHeader, ProductsSection, ResultsInfo, FilterSidebar, SortControls, and ProductsStyles
    - Simplifies imports across product listing pages and related components

---
+++ app/products/components/index.ts
@@ -0,0 +1,6 @@
+export { default as PageHeader } from "./PageHeader";
+export { default as ProductsSection } from "./ProductsSection";
+export { default as ResultsInfo } from "./ResultsInfo";
+export { default as FilterSidebar } from "./FilterSidebar";
+export { default as SortControls } from "./SortControls";
+export { default as ProductsStyles } from "./ProductsStyles";
