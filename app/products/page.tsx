4c2ce69a78b65a69c8507b3eda3c39fb8870db0d
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-11 06:02:03 +0200

    feat(filters): add shared filter, sort options, and default values constants

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -33,41 +33,20 @@ export default function ProductsPage() {
       
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col lg:flex-row gap-8">
-          {isArabic ? (
-            <>
-              <div className="flex-1">
-                <SortControls filters={filters} handleFilterChange={handleFilterChange} />
-                <ProductsSection filters={filters} />
-              </div>
-              <FilterSidebar
-                filters={filters}
-                showMobileFilters={showMobileFilters}
-                showCategoryDropdown={showCategoryDropdown}
-                setShowMobileFilters={setShowMobileFilters}
-                setShowCategoryDropdown={setShowCategoryDropdown}
-                handleFilterChange={handleFilterChange}
-                handleArrayFilterChange={handleArrayFilterChange}
-                clearAllFilters={clearAllFilters}
-              />
-            </>
-          ) : (
-            <>
-              <FilterSidebar
-                filters={filters}
-                showMobileFilters={showMobileFilters}
-                showCategoryDropdown={showCategoryDropdown}
-                setShowMobileFilters={setShowMobileFilters}
-                setShowCategoryDropdown={setShowCategoryDropdown}
-                handleFilterChange={handleFilterChange}
-                handleArrayFilterChange={handleArrayFilterChange}
-                clearAllFilters={clearAllFilters}
-              />
-              <div className="flex-1">
-                <SortControls filters={filters} handleFilterChange={handleFilterChange} />
-                <ProductsSection filters={filters} />
-              </div>
-            </>
-          )}
+          <FilterSidebar
+            filters={filters}
+            showMobileFilters={showMobileFilters}
+            showCategoryDropdown={showCategoryDropdown}
+            setShowMobileFilters={setShowMobileFilters}
+            setShowCategoryDropdown={setShowCategoryDropdown}
+            handleFilterChange={handleFilterChange}
+            handleArrayFilterChange={handleArrayFilterChange}
+            clearAllFilters={clearAllFilters}
+          />
+          <div className="flex-1">
+            <SortControls filters={filters} handleFilterChange={handleFilterChange} />
+            <ProductsSection filters={filters} />
+          </div>
         </div>
       </div>
       
