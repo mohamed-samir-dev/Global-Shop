f4caf354b95426effca3678904dc67c2c636d715
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-25 21:21:47 +0200

    Add order-success components index

---
+++ app/order-success/components/index.ts
@@ -0,0 +1,8 @@
+export { default as SuccessHeader } from './SuccessHeader';
+export { default as OrderItemsList } from './OrderItemsList';
+export { default as ShippingAddressCard } from './ShippingAddressCard';
+export { default as OrderSummaryCard } from './OrderSummaryCard';
+export { default as NextStepsCard } from './NextStepsCard';
+export { default as ActionButtons } from './ActionButtons';
+export { default as LoadingState } from './LoadingState';
+export { default as OrderNotFound } from './OrderNotFound';
