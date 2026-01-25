032e2ee89ce551f0215317b3880d04b586debc8d
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-25 21:21:48 +0200

    Add checkout constants

---
+++ constants/checkout.ts
@@ -0,0 +1,6 @@
+export const EGYPT_GOVERNORATES = [
+  'Cairo', 'Giza', 'Alexandria', 'Qalyubia', 'Port Said', 'Suez', 'Luxor', 'Aswan',
+  'Asyut', 'Beheira', 'Beni Suef', 'Dakahlia', 'Damietta', 'Faiyum', 'Gharbia',
+  'Ismailia', 'Kafr El Sheikh', 'Matrouh', 'Minya', 'Monufia', 'New Valley',
+  'North Sinai', 'Qena', 'Red Sea', 'Sharqia', 'Sohag', 'South Sinai'
+];
